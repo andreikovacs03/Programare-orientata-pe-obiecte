677b9129144ae6db75fef31e4ad31d94976133ed
Andrei K <andreijok678@yahoo.com>
2021-02-25 16:40:49 +0200

    Add project files.

---
+++ ComplexNumbers/Complex.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    public class Complex
+    {
+        double real;
+        double imaginary;
+
+        public double Real { get { return real; } }
+        public double Imaginary { get { return imaginary; } }
+
+        public Complex(double real, double imaginary = 0)
+        {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public override string ToString()
+        {
+            return imaginary == 0 ? $"{real}" : $"{real} {(imaginary > 0 ? "+" : "-")} {Math.Abs(imaginary)}i";
+        }
+
+        public Complex Add(Complex number)
+        {
+            return new Complex(real + number.real, imaginary + number.imaginary);
+        }
+
+        public Complex Subtract(Complex number)
+        {
+            return new Complex(real - number.real, imaginary - number.imaginary);
+        }
+
+        public Complex Multiply(Complex number)
+        {
+            return new Complex(real * number.real - imaginary * number.imaginary,
+                               real * number.imaginary - imaginary * number.real);
+        }
+
+        public double Abs()
+        {
+            return Math.Sqrt(real * real + imaginary * imaginary);
+        }
+
+        public double Phase()
+        {
+            return Math.Atan2(imaginary, real);
+        }
+    }
+}
---
+++ ComplexNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ComplexNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c1 = new Complex(1, 2);
+            Complex c2 = new Complex(3, -2);
+            Complex c3 = new Complex(-3);
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+
+            Console.WriteLine(c1.Abs());
+            Console.WriteLine(c1.Phase());
+        }
+    }
+}
