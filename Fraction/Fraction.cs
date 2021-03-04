faaa219f6ff0b9b5f0ccdcb784201a6dcea8c472
Andrei K <andreikovacs03@gmail.com>
2021-03-04 19:51:21 +0200

    ➕ Added Fraction

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Fraction
+{
+    public class Fraction
+    {
+        int num { get; }
+        int den { get; }
+
+        public Fraction(int num, int den)
+        {
+            if (den == 0)
+                throw new ArgumentException("Denominator cannot be zero.", nameof(den));
+
+            this.num = num;
+            this.den = den;
+        }
+
+        public Fraction Add(Fraction number) => new Fraction(num * number.den + number.num * den, den * number.den);
+
+        public Fraction Subtract(Fraction number) => Add(new Fraction(-number.num, number.den));
+
+        public Fraction Multiply(Fraction number) => new Fraction(num * number.num, den * number.den);
+
+        public Fraction Divide(Fraction number)
+        {
+            if (number.num == 0)
+                throw new DivideByZeroException();
+            return new Fraction(num * number.den, den * number.num);
+        }
+
+        public static Fraction operator +(Fraction a, Fraction b) => a.Add(b);
+
+        public static Fraction operator -(Fraction a, Fraction b) => a.Subtract(b);
+
+        public static Fraction operator *(Fraction a, Fraction b) => a.Multiply(b);
+
+        public static Fraction operator /(Fraction a, Fraction b) => a.Divide(b);
+
+        public override string ToString() => $"{num} / {den}";
+    }
+}
---
+++ Fraction/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fraction
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var a = new Fraction(2, 3);
+            var b = new Fraction(5, 9);
+
+            Console.WriteLine(a.Add(b));
+            Console.WriteLine(a.Subtract(b));
+            Console.WriteLine(b.Multiply(b));
+            Console.WriteLine(a.Divide(b));
+            Console.WriteLine(b / a);
+        }
+    }
+}
